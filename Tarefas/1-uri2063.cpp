0b4508a957218e11bf50e0cd48d806c717e2c486
gabrielmoro <gabrielhmoro@gmail.com>
2019-10-12 01:56:09 -0300

    ...

--- Tarefas/1-uri2063.cpp
+++ Tarefas/1-uri2063.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
+#include <cstring>
+
+#define MAX 112
 
 using namespace std;
 
-int mdc(int a, int b){
-    if (b == 0) return a;
-    return mdc(b, a % b);
+int b[MAX], c[MAX], v[MAX];
+
+int gcd(int a, int b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+
+int lcm(int a, int b){
+  return (a * b)/gcd(a, b);
 }
 
 int main(){
-  int n, res = 0;
+  int i, j, n, res = 0;
+
+  memset(c, 0, sizeof(c));
+  memset(v, 0, sizeof(v));
 
   cin >> n;
-  int b[n];
 
-  for(int i = 0; i < n; i++)
+  for(i = 0; i < n; i++){
     cin >> b[i];
+    b[i]--;  
+  }
 
-  for(int i = 0; i < n; i++)
-	continue
+  for(i = 0; i < n; i++){
+    j = i;
+    while(v[j] == 0){
+      cout << j + 1 << ' ';
+      v[j]++;
+      j = b[j];
+    }
+  }
 
-  cout << res << '\n';
+  //for (i = 0; i < n; i++)
+  //  cout << c[i] << ' ';  
+
+  cout << res << endl;
 
   return 0;
 }
