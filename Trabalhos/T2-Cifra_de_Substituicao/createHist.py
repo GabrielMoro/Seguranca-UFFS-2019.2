eb4949a7a02703ae29ab4c0fd9bb5aae9c63b321
gabrielmoro <gabrielhmoro@gmail.com>
2019-10-19 12:29:32 -0300

    Organizando

---
+++ Trabalhos/T2-Cifra_de_Substituicao/createHist.py
@@ -0,0 +1,27 @@
+import json
+import string
+
+def createHist(filename):
+    hist = {}
+
+    with open(filename, 'r', encoding = "utf8") as f:
+        for word in f.readlines():
+            for c in word:
+                if(c not in hist):
+                    hist[c] = 1
+                else:
+                    hist[c] += 1
+        f.close()
+
+    hist = {k : v for k, v in sorted(hist.items(), key = lambda x: x[1], reverse = True)}
+    return hist
+
+
+def saveHist(hist):
+    with open('frequency.json', 'w') as f:
+        json.dump(hist, f)
+        f.close()
+
+
+hist = createHist('source.txt')
+saveHist(hist)
