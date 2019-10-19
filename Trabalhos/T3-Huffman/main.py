245f5b1aa681d22712d9d5a70a126e159d0b1a1c
gabrielmoro <gabrielhmoro@gmail.com>
2019-10-19 16:20:40 -0300

    Começando a função de compressão

--- Trabalhos/T3-Huffman/main.py
+++ Trabalhos/T3-Huffman/main.py
@@ -1,3 +1,14 @@
+import heapq
+
+
+class HeapNode:
+    def __init__(self, values = (), left = None, right = None):
+        if not isinstance(values, tuple):
+            raise TypeError("Values must be a tuple")
+        self.values = values
+        self.left = left
+        self.right = right
+
 def createHist(txt):
     h = {}
     for c in txt:
@@ -5,14 +16,19 @@ def createHist(txt):
             h[c] = 1
         else:
             h[c] += 1
-    h = {k: v for k, v in sorted(h.items(), key=lambda x: x[1], reverse=True)}
+    h = {k: v for k, v in sorted(h.items(), key=lambda x: x[1])}
     return h
 
 
-with open('.txt', 'r') as f:
+def compression(hist):
+    compressed = ''
+
+    return compressed
+
+
+with open('.txt', 'r', encoding="utf8") as f:
     txt = f.read()
     txt = txt.lower()
     f.close()
 
 hist = createHist(txt)
-print(hist)
