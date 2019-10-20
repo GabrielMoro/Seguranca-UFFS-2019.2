9afac517c1526fd28cb3222608320db9ff539f0b
gabrielmoro <gabrielhmoro@gmail.com>
2019-10-19 22:17:02 -0300

    ...

--- Trabalhos/T3-Huffman/main.py
+++ Trabalhos/T3-Huffman/main.py
@@ -1,6 +1,41 @@
 import heapq
 
 
+# Source: https://gist.github.com/bhrigu123/a0e50b1b468cff905346b451ab3a2c39#file-huffmancoding-py
+def pad_encoded_text(encoded_text):
+		extra_padding = 8 - len(encoded_text) % 8
+		for i in range(extra_padding):
+			encoded_text += "0"
+
+		padded_info = "{0:08b}".format(extra_padding)
+		encoded_text = padded_info + encoded_text
+		return encoded_text
+
+
+# Source: https://gist.github.com/bhrigu123/a0e50b1b468cff905346b451ab3a2c39#file-huffmancoding-py
+def remove_padding(padded_encoded_text):
+		padded_info = padded_encoded_text[:8]
+		extra_padding = int(padded_info, 2)
+
+		padded_encoded_text = padded_encoded_text[8:]
+		encoded_text = padded_encoded_text[:-1*extra_padding]
+
+		return encoded_text
+
+
+# Source: https://gist.github.com/bhrigu123/a0e50b1b468cff905346b451ab3a2c39#file-huffmancoding-py
+def get_byte_array(padded_encoded_text):
+    if(len(padded_encoded_text) % 8 != 0):
+        print("Encoded text not padded properly")
+        exit(0)
+
+    b = bytearray()
+    for i in range(0, len(padded_encoded_text), 8):
+        byte = padded_encoded_text[i:i+8]
+        b.append(int(byte, 2))
+    return b
+
+
 def createHist(txt):
     h = {}
     for c in txt:
@@ -26,29 +61,6 @@ def createHeap(hist):
 
     return sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
 
-# Source: https://gist.github.com/bhrigu123/a0e50b1b468cff905346b451ab3a2c39#file-huffmancoding-py
-def pad_encoded_text(encoded_text):
-		extra_padding = 8 - len(encoded_text) % 8
-		for i in range(extra_padding):
-			encoded_text += "0"
-
-		padded_info = "{0:08b}".format(extra_padding)
-		encoded_text = padded_info + encoded_text
-		return encoded_text
-
-
-# Source: https://gist.github.com/bhrigu123/a0e50b1b468cff905346b451ab3a2c39#file-huffmancoding-py
-def get_byte_array(padded_encoded_text):
-    if(len(padded_encoded_text) % 8 != 0):
-        print("Encoded text not padded properly")
-        exit(0)
-
-    b = bytearray()
-    for i in range(0, len(padded_encoded_text), 8):
-        byte = padded_encoded_text[i:i+8]
-        b.append(int(byte, 2))
-    return b
-
 
 def compress(txt, hist):
     c = ''
@@ -62,7 +74,13 @@ def compress(txt, hist):
     print(get_byte_array(pad_encoded_text(c)))
     with open('.bin', 'wb') as f:
         f.write(get_byte_array(pad_encoded_text(c)))
+        f.close()
 
+    return heap
+
+
+def decompress(txt):
+    return
 
 with open('.txt', 'r', encoding="utf8") as f:
     txt = f.read()
@@ -71,4 +89,4 @@ with open('.txt', 'r', encoding="utf8") as f:
 
 hist = createHist(txt)
 
-compress(txt, hist)
+heap = compress(txt, hist)
