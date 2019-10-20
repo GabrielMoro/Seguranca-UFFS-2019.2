f217d7806f39d7c3e999c4672ab39e1d75fd67d3
gabrielmoro <gabrielhmoro@gmail.com>
2019-10-19 23:13:27 -0300

    Corrigindo uma burrice

--- Tarefas/5-caesar.py
+++ Tarefas/5-caesar.py
@@ -12,7 +12,7 @@ def encode(key, txt):
 def decode(key, txt):
     decoded = ''
     for i in range(len(txt)):
-        decoded += chr((ord(txt[i]) - key) % 256)
+        decoded += chr((ord(txt[i]) - ord(key)) % 256)
     return decoded
 
 
@@ -35,10 +35,9 @@ with open('sample.txt', 'r') as f:
     txt = f.read()
     f.close()
 
-print(encode(key, txt))
-
 with open('caesar.txt', 'w', encoding="utf-8") as f:
     f.write('Key: ' + str(key))
     f.write('\n\nOriginal: ' + txt)
     f.write('\n\nEncoded: ' + encode(key, txt))
+    f.write('\n\nDecoded: ' + decode(key, encode(key, txt)))
     f.close()
