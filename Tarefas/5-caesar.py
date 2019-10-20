d7f9960344860c18a31532c5d3da60dbf0887d5f
gabrielmoro <gabrielhmoro@gmail.com>
2019-10-19 22:59:05 -0300

    Pronto

--- Tarefas/5-caesar.py
+++ Tarefas/5-caesar.py
@@ -5,7 +5,7 @@ import platform
 def encode(key, txt):
     coded = ''
     for i in range(len(txt)):
-        coded += chr((ord(txt[i]) + key) % 256)
+        coded += chr((ord(txt[i]) + ord(key)) % 256)
     return coded
 
 
@@ -29,7 +29,7 @@ def clearScreen():
 
 
 clearScreen()
-key = ord(input('\nKey: '))
+key = input('\nKey: ')
 
 with open('sample.txt', 'r') as f:
     txt = f.read()
@@ -39,6 +39,6 @@ print(encode(key, txt))
 
 with open('caesar.txt', 'w', encoding="utf-8") as f:
     f.write('Key: ' + str(key))
-    f.write('\nOriginal: ' + txt)
+    f.write('\n\nOriginal: ' + txt)
     f.write('\n\nEncoded: ' + encode(key, txt))
     f.close()
