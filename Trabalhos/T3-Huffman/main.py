6c87173e9fb41ae80423ff0bd7e7f6679f6c5127
gabrielmoro <gabrielhmoro@gmail.com>
2019-10-19 22:03:11 -0300

    Comprimindo, falta descompressão

--- Trabalhos/T3-Huffman/main.py
+++ Trabalhos/T3-Huffman/main.py
@@ -26,6 +26,29 @@ def createHeap(hist):
 
     return sorted(heapq.heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
 
+# Source: https://gist.github.com/bhrigu123/a0e50b1b468cff905346b451ab3a2c39#file-huffmancoding-py
+def pad_encoded_text(encoded_text):
+		extra_padding = 8 - len(encoded_text) % 8
+		for i in range(extra_padding):
+			encoded_text += "0"
+
+		padded_info = "{0:08b}".format(extra_padding)
+		encoded_text = padded_info + encoded_text
+		return encoded_text
+
+
+# Source: https://gist.github.com/bhrigu123/a0e50b1b468cff905346b451ab3a2c39#file-huffmancoding-py
+def get_byte_array(padded_encoded_text):
+    if(len(padded_encoded_text) % 8 != 0):
+        print("Encoded text not padded properly")
+        exit(0)
+
+    b = bytearray()
+    for i in range(0, len(padded_encoded_text), 8):
+        byte = padded_encoded_text[i:i+8]
+        b.append(int(byte, 2))
+    return b
+
 
 def compress(txt, hist):
     c = ''
@@ -36,6 +59,9 @@ def compress(txt, hist):
     # txt = list(txt)
     for i in range(len(txt)):
         c += aux[txt[i]]
+    print(get_byte_array(pad_encoded_text(c)))
+    with open('.bin', 'wb') as f:
+        f.write(get_byte_array(pad_encoded_text(c)))
 
 
 with open('.txt', 'r', encoding="utf8") as f:
